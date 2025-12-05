2480af977c475750254869465e5a1e7306367cbf
6gaurav13 <6gaurav13@gmail.com>
2025-12-05 18:26:33 +0530

    JPA Query Modification

--- src/main/java/com/analytics/reporting/service/ReportService.java
+++ src/main/java/com/analytics/reporting/service/ReportService.java
@@ -1,13 +1,48 @@
 package com.analytics.reporting.service;
 
+import com.analytics.reporting.repo.ReportingJPA;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Service
+@RequiredArgsConstructor
 public class ReportService {
+
+    private final ReportingJPA reportingJPA;
+
 
     public Object getReport(String appId)
     {
-        return "";
+        Map<String,Object> response = new HashMap<>();
+
+        long totalUsers = reportingJPA.countByAppId(appId);
+        long uniqueUsers = reportingJPA.countUniqueUsers(appId);
+        long mobileUsers = reportingJPA.countByAppIdAndDeviceType(appId,"mobile");
+        long desktopUsers = reportingJPA.countByAppIdAndDeviceType(appId,"desktop");
+
+        Map<String,Long> deviceMap = new HashMap<>();
+        deviceMap.put("mobile",mobileUsers);
+        deviceMap.put("desktop",desktopUsers);
+        //contains ["event1":2,"event2":3]
+        List<Object[]> eventCounts = reportingJPA.countUsersPerEvent(appId);
+        Map<String,Long> eventMap = new HashMap<>();
+
+        for(Object[] event:eventCounts)
+            eventMap.put(event[0].toString(),(Long)event[1]);
+
+        response.put("appId",appId);
+        response.put("totalUsers",totalUsers);
+        response.put("uniqueUsers",uniqueUsers);
+        response.put("devices",deviceMap);
+        response.put("mobile",mobileUsers);
+
+
+
+        return response;
     }
 
 }
